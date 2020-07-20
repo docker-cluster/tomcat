a06536e9edc88fec362a368a186f99f70f865dc2
Dongdong Zhang <dongdzha@cisco.com>
2020-07-20 23:58:20 +0800

    20200720

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ if [ $? -ne 0 ]
 then
   tar -zxf webapps.tar.gz
 fi
+#allow all ip
+sed -i 's/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/allow=".*"/' context.xml
 echo "***************************webapp unzipped***************************"
 
 echo "***************************start to init tomcat***************************"
 chmod a+r conf/*
+
 docker-compose down
 docker-compose up -d
 echo "***************************tomcat containers inited***************************"
