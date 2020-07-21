2889a04bc8abb28aa5681ebd33f2d21bb4bd9f9f
zhangddjs <1005155946@qq.com>
2020-07-21 23:38:02 +0800

    20200721

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 echo "***************************start to unzip webapp***************************"
-ls | grep '^webapps$'
+ls | grep '^ego-portal1$'
 if [ $? -ne 0 ]
 then
   tar -zxf webapps.tar.gz
   #allow all ip
   sed -i 's/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/allow=".*"/' ./webapps/manager/META-INF/context.xml
 fi
+mv webapps ego-portal1
+ls | grep '^ego-portal2$'
+if [ $? -ne 0 ]
+then
+  tar -zxf webapps.tar.gz
+  #allow all ip
+  sed -i 's/allow="127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1"/allow=".*"/' ./webapps/manager/META-INF/context.xml
+fi
+mv webapps ego-portal1
 echo "***************************webapp unzipped***************************"
 
 echo "***************************start to init tomcat***************************"
